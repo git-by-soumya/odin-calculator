db1e7b9fb6bb94c68c072f0343559e98c5fdb0e6
soumya <197779975+git-by-soumya@users.noreply.github.com>
2025-03-13 02:05:44 +0530

    Create some variables and the function operate()
    The variables will store an operator and two numbers. The function will
    use them.

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let operandOne;
+let operator;
+let operandTwo;
+
+function operate(sign, num1, num2) {
+    let result;
+    switch(sign) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+        default:
+            result = "Wrong sign!";
+    }
+    return result;
+}
