124d76a5433ff39d7e5371f49251538f1d469cdc
soumya <197779975+git-by-soumya@users.noreply.github.com>
2025-03-13 02:55:16 +0530

    Create functions to respond to digit button press

--- script.js
+++ script.js
@@ -38,3 +38,35 @@ function operate(sign, num1, num2) {
     }
     return result;
 }
+
+const doc = document;
+let displayContent = "";
+
+function display(content) {
+    const display = document.querySelector("p");
+    display.textContent = content;
+}
+
+function digitPressHandler(event) {
+    const target = event.target;
+    if(target.tagName === "BUTTON") {
+        const buttonContent = target.textContent;
+        switch(buttonContent) {
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                displayContent = displayContent + buttonContent;
+                display(displayContent);
+                break;
+        }
+    }
+}
+
+doc.addEventListener("click", digitPressHandler);
