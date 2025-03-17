d0d232af98ccedd432ec7f0603abc477112721f4
soumya <197779975+git-by-soumya@users.noreply.github.com>
2025-03-18 02:48:58 +0530

    Refactor digit press handling logic
    Packed digit press handling logic into a dedicated function, which is
    now called from the switch statement.
    
    Wrote pseudocode for handling other button presses, including:
     * Operator buttons
     * Equals to button
     * Unimplemented features such as decimal point and backspace buttons
    
    These changes improve code organization and set the groundwork for
    future implementations.
    
    Also, updated data clearing logic for conciseness.

--- script.js
+++ script.js
@@ -65,7 +65,173 @@ function operate(sign, num1, num2) {
 }
 
 const doc = document;
-let displayContent = "";
+//let displayContent = "";
+
+function digitPressHandler(digitPressed, display) {
+    if (operandOne !== "" && operator !== "" && operandTwo !== "") {
+        operandTwo = operandTwo.toString() + digitPressed;
+        display.textContent = operandOne + operator + operandTwo;
+    }
+    else if (operandOne !== "" && operator === "=" && operandTwo === "") {
+        operandOne = digitPressed;
+        operator = "";
+        display.textContent = operandOne;
+    }
+    else if (operandOne !== "" && 
+        ["+", "-", "*", "/"].includes(operator) && 
+        operandTwo === "") 
+        {
+        operandTwo = digitPressed;
+        display.textContent = operandOne + operator + operandTwo;
+    }
+    else if (operandOne !== "" && operator === "" && operandTwo === "") {
+        operandOne = operandOne.toString() + digitPressed;
+        display.textContent = operandOne;
+    }
+    else if(operandOne === "" && operator === "" && operandTwo === "") {
+        operandOne = digitPressed;
+        display.textContent = operandOne;
+    } 
+}
+
+function operatorPressHandler() {
+    /*
+    if num1, operator, num2 are filled and division by zero is 
+    attempted(operator is division and num2 is zero at the same time)
+        display 'you nutter'
+        empty num1, operator, num2
+
+    else if num1, operator, num2 are filled and no division by 
+    zero(operator and num2 aren't division and zero, respectively, 
+    at the same time)
+        get result of operation on num1, operator, num2
+        overwrite num1 with result
+        overwrite operator with operatorPress(+ - * /)
+        empty num2
+        display num1 operator, i.e., display result operator
+
+    else if num1 and operator is filled, num2 is empty
+        overwrite operator with operatorPress(+ - * /)
+        display num1 operator
+
+    else if num1 is filled, operator and num2 are empty
+        overwrite operator with operatorPress(+ - * /)
+        display num1 operator
+
+    else if num1, operator, num2 are empty
+        overwrite num1 with zero or "0"
+        overwrite operator with operatorPress(+ - * /)
+        display num1 operator, i.e., display 0 operator
+    */
+}
+
+function equalsToPressHandler() {
+    /*
+    if num1, operator, num2 are filled and division by zero is 
+    attempted(operator is division and num2 is zero at the same time)
+        display 'you nutter'
+        empty num1, operator, num2
+
+    else if num1, operator, num2 are filled and no division by 
+    zero(operator and num2 aren't division and zero, respectively, 
+    at the same time)
+        get result of operation on num1, operator, num2
+        overwrite num1 with result
+        overwrite operator with operatorPress(=)
+        empty num2
+        display num1, i.e., display result
+
+    else if num1 and operator is filled, num2 is empty
+        overwrite operator with operatorPress(=)
+        display num1
+
+    else if num1 is filled, operator and num2 are empty
+        overwrite operator with operatorPress(=)
+        display num1
+    */
+}
+
+function operatorAndEqualsToPressHandler() { //too long???
+    /*
+    function display(display) {
+        if operator is + - * /
+            display num1 operator, i.e., display result operator
+        else if operator is =
+            display num1, i.e., display result
+    }
+
+    if num1, operator, num2 are filled and division by zero is 
+    attempted(operator is division and num2 is zero at the same time)
+        display 'you nutter'
+        empty num1, operator, num2
+
+    else if num1, operator, num2 are filled and no division by 
+    zero(operator and num2 aren't division and zero, respectively, 
+    at the same time)
+        get result of operation on num1, operator, num2
+        overwrite num1 with result
+        overwrite operator with operatorPress(+ - * / =)
+        empty num2
+        display(num1, operator, display)
+
+    else if num1 and operator is filled, num2 is empty
+        overwrite operator with operatorPress(+ - * / =)
+        display(num1, operator, display)
+
+    else if num1 is filled, operator and num2 are empty
+        overwrite operator with operatorPress(+ - * / =)
+        display(num1, operator, display)
+
+    else if num1, operator, num2 are empty and operatorPress is + - * /, not =
+        overwrite num1 with zero or "0"
+        overwrite operator with operatorPress(+ - * /)
+        display num1 operator, i.e., display 0 operator
+    */
+}
+
+function clearPressHandler() {
+    /*
+    display 0 or "0"
+    empty num1, operator, num2
+    */
+}
+
+function backspacePressHandler() {
+    /*
+    if num1, operator, num2 are filled
+        remove last digit of num2, i.e., overwrite num2 with num2.slice(0, -1)
+        display num1 operator num2
+
+    else if num1 and operator is filled, num2 is empty
+        empty operator
+        display num1
+
+    else if num1 is filled, operator and num2 are empty
+        remove last digit of num1, i.e., overwrite num1 with num1.slice(0, -1)
+
+        if num1 is filled
+            display num1
+        else
+            display 0 or "0"
+    */
+}
+
+function decimalPointPressHandler() {
+    /*
+    if num1, operator, num2 are filled and num2 doesn't have a decimal point
+        append a decimal point to num2
+
+    else if num1 and operator is filled, num2 is empty
+        overwrite num2 with "0."
+
+    else if num1 is filled, operator and num2 are empty, and num1 doesn't have 
+    a decimal point
+        append a decimal point to num2
+    
+    else if num1, operator, num2 are empty
+        overwrite num1 with "0."
+    */
+}
 
 function buttonPressHandler(event) {
     const target = event.target;
@@ -85,123 +251,87 @@ function buttonPressHandler(event) {
             case "7":
             case "8":
             case "9":
-                if(operandOne !== "") {
-                    if(operator !== "") {
-                        if(operandTwo !== "") {
-                            operandTwo = 
-                            operandTwo.toString().concat(buttonContent);
-                            display.textContent = 
-                            operandOne + operator + operandTwo;
-                        }
-                        else if(operandTwo === "") {
-                            if(operator === "=") {
-                                operandOne = buttonContent;
-                                operator = "";
-                                display.textContent = operandOne;
-                            }
-                            else if(operator !== "=") {
-                                operandTwo = buttonContent;
-                                display.textContent = 
-                                operandOne + operator + operandTwo;
-                            }
-                        }
-                    }
-                    else if(operator === "") {
-                        operandOne = 
-                        operandOne.toString().concat(buttonContent);
-                        display.textContent = operandOne;
-                    }
-                }
-                else if(operandOne === "") {
-                    operandOne = buttonContent;
-                    display.textContent = operandOne;
-                }
+                digitPressHandler(buttonContent, display);
                 break;
             case "+":
             case "-":
             case "*":
             case "/":
-                if(buttonContent !== "=") {
-                    if(operandOne !== "") {
-                        if(operator !== "") {
-                            if(operandTwo !== "") {
-                                if(operator === "/" && +operandTwo === 0)
-                                {
+                if (buttonContent !== "=") {
+                    if (operandOne !== "") {
+                        if (operator !== "") {
+                            if (operandTwo !== "") {
+                                if (operator === "/" && +operandTwo === 0) {
                                     display.textContent = "you nutter";
-                                    operandOne = "";
-                                    operator = "";
-                                    operandTwo = "";
+                                    operandOne = operator = operandTwo = "";
                                 }
                                 else {
-                                    let result = 
-                                    operate(operator, +operandOne, +operandTwo);
+                                    let result =
+                                        operate(operator, +operandOne, +operandTwo);
                                     operandOne = result;
                                     operator = buttonContent;
                                     operandTwo = "";
-                                    display.textContent = 
-                                    operandOne.toString().concat(operator);
-                                }                                        
+                                    display.textContent =
+                                        operandOne.toString().concat(operator);
+                                }
                             }
-                            else if(operandTwo === "") {
+                            else if (operandTwo === "") {
                                 operator = buttonContent;
-                                display.textContent = 
-                                operandOne.toString().concat(operator);
+                                display.textContent =
+                                    operandOne.toString().concat(operator);
                             }
                         }
-                        else if(operator === "") {
+                        else if (operator === "") {
                             operator = buttonContent;
-                            display.textContent = 
-                            operandOne.toString().concat(operator);                            
+                            display.textContent =
+                                operandOne.toString().concat(operator);
                         }
                     }
-                    else if(operandOne === "") {
+                    else if (operandOne === "") {
                         operandOne = "0";
                         operator = buttonContent;
-                        display.textContent = 
-                        operandOne.toString().concat(operator);
+                        display.textContent =
+                            operandOne.toString().concat(operator);
                     }
                 }
                 break;
             case "=":
-                if(buttonContent === "=") {
-                    if(operandOne !== "") {
-                        if(operator !== "") {
-                            if(operandTwo !== "") {
-                                if(operator === "/" && +operandTwo === 0) {
+                if (buttonContent === "=") {
+                    if (operandOne !== "") {
+                        if (operator !== "") {
+                            if (operandTwo !== "") {
+                                if (operator === "/" && +operandTwo === 0) {
                                     display.textContent = "you nutter";
-                                    operandOne = "";
-                                    operator = "";
-                                    operandTwo = "";                                    
+                                    operandOne = operator = operandTwo = "";
                                 }
                                 else {
-                                    let result = 
-                                    operate(operator, +operandOne, +operandTwo);
+                                    let result =
+                                        operate(operator, +operandOne, +operandTwo);
                                     operandOne = result;
                                     operator = buttonContent;
                                     operandTwo = "";
                                     display.textContent = operandOne;
                                 }
                             }
-                            else if(operandTwo === "") {
+                            else if (operandTwo === "") {
                                 operator = buttonContent;
                                 display.textContent = operandOne;
                             }
                         }
-                        else if(operator === "") {
+                        else if (operator === "") {
                             operator = buttonContent;
                             display.textContent = operandOne;
                         }
                     }
-                    else if(operandOne === "") {
+                    else if (operandOne === "") {
                         //do nothing
                     }
                 }
                 break;
             case "Clear":
-                display.textContent = 0;
-                operandOne = "";
-                operator = "";
-                operandTwo = "";          
+                display.textContent = "0";
+                operandOne = operator = operandTwo = "";
+                break;
         }
     }
 }
